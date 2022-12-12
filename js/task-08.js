04c175ed4349612f9976abfa8f394d865f643c31
Pibodee <mrbodyanuch@gmail.com>
2022-12-12 23:10:11 +0200

    task 8 checked

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const {
+        elements: { email, password }
+    } = evt.currentTarget;
+    
+    if (email.value === '' || password.value === '') {
+        return alert('Всі поля мають бути заповнені')
+    }
+    console.log(`${email.name}: ${email.value}, ${password.name}: ${password.value}`);
+    evt.currentTarget.reset();
+})
