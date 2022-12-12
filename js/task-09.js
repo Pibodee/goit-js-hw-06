74687579c3350188c1f990e4b60323e0db868bb4
Pibodee <mrbodyanuch@gmail.com>
2022-12-12 23:19:19 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const button = document.querySelector('.change-color');
+const text = document.querySelector('.color')
+
+
+button.addEventListener('click', () => {
+  text.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = text.textContent;
+})
